797c8ff83cc405732f2e235a4678b8395e81e75b
sflamir <134074145+sflamir@users.noreply.github.com>
2024-01-16 07:52:51 -1200

    Flamir
    WebSite Service FLamir

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+
+window.onscroll = function(){
+    efectoHabilidades();
+} 
